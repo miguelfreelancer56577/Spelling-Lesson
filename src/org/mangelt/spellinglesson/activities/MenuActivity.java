6d492415c3c89db1512a7a49182f7d26b8aff945
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2017-10-20 13:38:06 -0500

    I've completed the creation and insertion into the database.

--- src/org/mangelt/spellinglesson/activities/MenuActivity.java
+++ src/org/mangelt/spellinglesson/activities/MenuActivity.java
@@ -4,23 +4,33 @@ import org.mangelt.spellinglesson.R;
 import org.mangelt.spellinglesson.R.id;
 import org.mangelt.spellinglesson.activities.MainActivity.PlaceholderFragment;
 import org.mangelt.spellinglesson.activities.MainActivity.PlaceholderFragmentTest;
+import org.mangelt.spellinglesson.database.DataBase;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MenuActivity extends ActionBarActivity {
 
+	private final String log = MenuActivity.class.getSimpleName();
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_inflate_screen_menu);
+        DataBase db = new DataBase(this);
+        ContentValues values2 = db.retrieveSingleEntry(DataBase.TBLLESSONS, " id_lesson = 1 ", db.retrieveAllColumnNames(DataBase.TBLLESSONS));
+    	db.close();
+    	Log.i(log, "show record: " + values2.getAsInteger("id_lesson")+" "+values2.getAsString("description")+" ");
     }
 
 }
---
+++ src/org/mangelt/spellinglesson/database/DataBase.java
@@ -0,0 +1,113 @@
+package org.mangelt.spellinglesson.database;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.Visdrotech.library.sqlitehelp.main.SQLiteHelper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataBase extends SQLiteHelper{
+
+	public static final String NAMEBD = "spellinglesson";
+	private final String log = DataBase.class.getSimpleName();
+//	name of the tables
+	public static final String TBLLESSONS = "lessons";
+	public static final String TBLCATALOGSPANISH = "catalog_spanish";
+	public static final String TBLCATALOGENGLISH = "catalog_english";
+	public static final String TBLCATALOGWORDS = "words";
+	
+	public DataBase(Context contexto) {
+		super(contexto, NAMEBD);
+		createTables();
+    }
+	
+	public void createTables(){
+//		table lessons
+		if(super.isTablePresent(TBLLESSONS) == false){
+			ArrayList<String> columns = new ArrayList<String>(); 
+			columns.add("id_lesson@integer");
+			columns.add("description@text");
+			createStructureCols(TBLLESSONS, columns);
+			ArrayList<String> rows = new ArrayList<String>();
+			rows.add("1@5A");
+			populateTable(TBLLESSONS, columns, rows);
+		}
+//		table catalog_spanish
+		if(super.isTablePresent(TBLCATALOGSPANISH) == false){
+			ArrayList<String> columns = new ArrayList<String>(); 
+			columns.add("id_word_s@integer");
+			columns.add("description@text");
+			createStructureCols(TBLCATALOGSPANISH, columns);
+			ArrayList<String> rows = new ArrayList<String>();
+			rows.add("1@saltar");
+			rows.add("2@jaula");
+			populateTable(TBLCATALOGSPANISH, columns, rows);
+		}
+//		table catalog_english
+		if(super.isTablePresent(TBLCATALOGENGLISH) == false){
+			ArrayList<String> columns = new ArrayList<String>(); 
+			columns.add("id_word_e@integer");
+			columns.add("description@text");
+			createStructureCols(TBLCATALOGENGLISH, columns);
+			ArrayList<String> rows = new ArrayList<String>();
+			rows.add("1@leap");
+			rows.add("2@hurdle");
+			rows.add("3@jump");
+			rows.add("4@cage");
+			populateTable(TBLCATALOGENGLISH, columns, rows);
+		}
+//		table words
+		if(super.isTablePresent(TBLCATALOGWORDS) == false){
+			ArrayList<String> columns = new ArrayList<String>(); 
+			columns.add("id_lesson@integer");
+			columns.add("id_word_s@integer");
+			columns.add("id_word_e@integer");
+			createStructureCols(TBLCATALOGWORDS, columns);
+			ArrayList<String> rows = new ArrayList<String>();
+			rows.add("1@1@1");
+			rows.add("1@1@2");
+			rows.add("1@1@3");
+			rows.add("1@2@4");
+			populateTable(TBLCATALOGWORDS, columns, rows);
+		}
+		
+	}
+	public void createStructureCols(final String table, final ArrayList<String> columns){
+		ArrayList<String> cols = new ArrayList<String>();
+		HashMap<String, String> typeMap = new HashMap<String, String>();
+		for (String current : columns) {
+			String[] column = current.split("@");
+			cols.add(column[0]);
+			typeMap.put(column[0], column[1]);
+		}
+		if(cols.size() > 0 && typeMap.size() > 0){
+			Log.d(log, "creating table:\n " + table + "\nwith columns:\n "  + columns);
+			super.createTable(table, cols, typeMap);
+		}
+			
+	}
+	
+	public void populateTable(final String table, final ArrayList<String> columns, final ArrayList<String> rows){
+		ArrayList<ContentValues> records = new ArrayList<ContentValues>();
+		for (String row : rows) {
+			String[] record = row.split("@");
+			ContentValues values = new ContentValues();
+			for (int i = 0; i < record.length; i++) {
+				if(i > columns.size())
+					break;
+				String value = record[i];
+				String nameColumn = columns.get(i).split("@")[0]; 
+				values.put(nameColumn, value);
+			}
+			records.add(values);
+		}
+		Log.d(log, "inserting into table:\n " + table + "\nrecords:\n "  + records);
+		super.insertMultipleEntries(table, records);
+	}
+}
